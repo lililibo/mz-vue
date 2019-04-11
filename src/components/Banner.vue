3e094840300f7a812790017555becd08686ffea7
libo <1293325947@qq.com>
2019-04-11 11:26:26 +0800

    swiper npm

---
+++ src/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="swiper-container billboards">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(item, index) in banners" :key="index">
+        <img :src="item.imgUrl" alt>
+      </div>
+    </div>
+    <!-- 分页器 -->
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+export default {
+  props: {
+    banners: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      swiper: null
+    }
+  },
+  updated () {
+    if (!this.swiper) {
+      this.swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.swiper-container {
+  width: 100%;
+  height: 2.1rem;
+  img {
+    width: 100%;
+  }
+  .swiper-pagination-bullet {
+    background: #fff;
+    opacity: 1;
+  }
+  .swiper-pagination-bullet-active {
+    background: red;
+  }
+  .swiper-pagination {
+    position: absolute;
+    width: 1.5rem;
+    height: 0.07rem;
+    bottom: .2rem;
+    left: 2.2rem;
+    .swiper-pagination-bullet {
+      width: .05rem;
+      height: .05rem;
+      border-radius: 100%;
+      margin-right: .03rem;
+      background: rgba(255, 255, 255, 0);
+      border: 1px solid white;
+    }
+    .swiper-pagination-bullet-active{
+      background: #fff;
+    }
+  }
+}
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    电影首页
+    <Banner :banners="bannerList"></Banner>
   </div>
 </template>
+
+<script>
+import Banner from '../components/Banner'
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      bannerList: []
+    }
+  },
+  components: {
+    Banner
+  },
+  methods: {
+    getBannerList () {
+      /**
+     * 请求轮播图的数据
+     */
+      axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=5889141', {
+        headers: {
+          'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15543712411546188226643"}',
+          'X-Host': 'mall.cfg.common-banner'
+        }
+      }).then(res => {
+        let data = res.data
+        if (data.status === 0) {
+          this.bannerList = data.data
+        } else {
+          alert(data.msg)
+        }
+      })
+    }
+  },
+  created () {
+    this.getBannerList()
+  }
+}
+</script>
